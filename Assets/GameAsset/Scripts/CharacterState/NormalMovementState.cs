df67f2d0f47384bcc13c933f8daf3c48b0429126
phongth <phongth@bap-ventures.com>
2021-10-22 10:36:59 +0700

    Update

--- Assets/GameAsset/Scripts/CharacterState/NormalMovementState.cs
+++ Assets/GameAsset/Scripts/CharacterState/NormalMovementState.cs
@@ -10,13 +10,25 @@ namespace Game
 {
     public class NormalMovementState : CharacterState
     {
+        [SerializeField] private float speedLerpRotate = .1f;
+        [SerializeField] private float moveSpeed = 1;
+        [SerializeField] private string speedParam = "Velocity";
+        
+        private Vector3 _posClickOld;
+        private Vector3 _deltaClick;
+        private Vector3 _lookDir;
+        private bool _isTouch = false;
+        private float _horizontalMove = 0;
+        
         private CharacterController _characterController = null;
+        private PlayerController _playerController;
 
         protected override void Start()
         {
             base.Start();
 
-            _characterController = this.GetComponentInBranch<CharacterController, CharacterController>();
+            _characterController = this.GetComponentInBranch<PlayerController, CharacterController>();
+            _playerController = this.GetComponentInBranch<PlayerController, PlayerController>();
         }
 
         public override void EnterState(float dt, CharacterState fromState)
@@ -31,12 +43,93 @@ namespace Game
 
         public override void UpdateBehaviour(float dt)
         {
+            if (_isTouch)
+            {
+                HandlingMovement();
+            }
+        }
+
+        public override void PostUpdateBehaviour(float dt)
+        {
+            if (CharacterStateController.Animator == null)
+            {
+                return;
+            }
+
+            if (CharacterStateController.Animator.runtimeAnimatorController == null)
+            {
+                return;
+            }
+
+            if (!CharacterStateController.Animator.gameObject.activeSelf)
+            {
+                return;
+            }
             
+            CharacterStateController.Animator.SetFloat(speedParam, _horizontalMove);
         }
 
         private void OnInputResponse(InputPhase phase, Vector3 inputPos)
         {
-            
+            switch (phase)
+            {
+                case InputPhase.Began:
+                    _posClickOld = inputPos;
+                    _isTouch = true;
+                    _horizontalMove = 1;
+                    break;
+                case InputPhase.Moved:
+                    _deltaClick = inputPos - _posClickOld;
+                    _posClickOld = inputPos;
+                    break;
+                case InputPhase.Ended:
+                    _deltaClick = Vector3.zero;
+                    _lookDir = Vector3.zero;
+                    _horizontalMove = 0;
+                    _isTouch = false;
+                    break;
+                default:
+                    break;
+            }
+        
+            HandlingInput();
+        }
+        
+        private void HandlingInput()
+        {
+            if (_deltaClick != Vector3.zero)
+            {
+                Vector3 deltaTouch = _deltaClick;
+                deltaTouch.z = deltaTouch.y;
+                deltaTouch.y = 0;
+
+                _lookDir += deltaTouch;
+                _lookDir = Vector3.ClampMagnitude(_lookDir, 150f);
+            }
+
+            if (_lookDir != Vector3.zero)
+            {
+                float angle = Mathf.Atan2(_lookDir.x, _lookDir.z) * Mathf.Rad2Deg;
+                //float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref _turnSmoothTime, speedLerpRotate);
+                //transform.rotation = Quaternion.Euler(0f, smoothAngle, 0);
+                _playerController.Rotation = Quaternion.Lerp(_playerController.Rotation, Quaternion.Euler(0f, angle, 0), speedLerpRotate);
+            }
+        }
+        
+        private void HandlingMovement()
+        {
+            float angle = Mathf.Atan2(_lookDir.x, _lookDir.z) * Mathf.Rad2Deg;
+            Vector3 moveDir = Quaternion.Euler(0, angle, 0) * Vector3.forward;
+            Vector3 forward = CacheTransform.forward * _horizontalMove;
+            if (_characterController.isGrounded == false)
+            {
+                forward.y += Physics.gravity.y;
+            }
+            else
+            {
+                forward.y = 0;
+            }
+            _characterController.Move(forward * (Time.fixedDeltaTime * moveSpeed));
         }
     }
 }
--- Assets/GameAsset/Scripts/GameState/PlayingState.cs
+++ Assets/GameAsset/Scripts/GameState/PlayingState.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Base;
+using Base.MessageSystem;
 using Base.Pattern;
 using UnityEngine;
 
@@ -7,9 +9,10 @@ namespace Game
 {
     public class PlayingState : GameState
     {
+        private InputAction inputAction => GameStateController.InputAction;
         public override void UpdateBehaviour(float dt)
         {
-            
+            Messenger.RaiseMessage(SystemMessage.Input, inputAction.Phase, inputAction.Position);
         }
     } 
 }
